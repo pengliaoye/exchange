d92e7e5398fbdeeac23317363aafe941e26b3e25
peng guo yong <wjpgy@126.com>
2013-04-15 18:29:21 +0800

    e

--- src/test/java/com/dm/exchange/HelloWorldTest.java
+++ src/test/java/com/dm/exchange/HelloWorldTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "WEB-INF/applicationContext.xml")
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/applicationContext.xml")
 public class HelloWorldTest {
 	@Test
 	public void testIsValidEmail() throws Exception {
