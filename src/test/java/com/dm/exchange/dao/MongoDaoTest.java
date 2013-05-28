85e94584558feb9bc75aae51e94ffa5a583b6e7b
guoyong peng <wjpgy@126.com>
2013-05-28 16:59:47 +0800

    c

---
+++ src/test/java/com/dm/exchange/dao/MongoDaoTest.java
@@ -0,0 +1,35 @@
+package com.dm.exchange.dao;
+
+import java.net.UnknownHostException;
+
+import org.junit.Test;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+public class MongoDaoTest {
+
+	@Test
+	public void testDb() {
+		try {
+			MongoClient mongoClient = new MongoClient();
+			DB db = mongoClient.getDB("test");
+			DBCollection coll = db.getCollection("test");
+			DBCursor cursor = coll.find();
+			try {
+				while (cursor.hasNext()) {
+					System.out.println(cursor.next());
+				}
+			} finally {
+				cursor.close();
+			}
+			mongoClient.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
