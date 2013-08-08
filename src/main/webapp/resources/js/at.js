d297160b8e2d595d21e7d2d5492c8d3d226ff9cd
guoyong peng <wjpgy@126.com>
2013-08-08 14:52:39 +0800

    e

--- src/main/webapp/resources/js/at.js
+++ src/main/webapp/resources/js/at.js
@@ -1,5 +1,7 @@
 YUI.add('atwho', function(Y) {
-    var Lang = Y.Lang;
+	
+    var Lang = Y.Lang,    
+    INPUT_NODE          = 'inputNode';
     
     function AtWho(config){
         AtWho.superclass.constructor.apply(this, arguments);
@@ -8,11 +10,50 @@ YUI.add('atwho', function(Y) {
     AtWho.NAME = "atWho";
     
     AtWho.ATTRS = {
-        
+        data : {
+        	value : null
+        }
     };
     
-    Y.extend(AtWho, Y.Widget, {
-        
+    AtWho.AT_DIV = "<div id='at-view' class='at-view'><ul id='at-view-ul'></ul></div>";
+    AtWho.DEFAULT_TPL = "<li data-value='${name}'>${name}</li>";
+    
+    Y.extend(AtWho, Y.Widget, {   
+    	
+	    initializer: function() {
+	    	var data = this.get("data");
+	    	if(Lang.isString(data)){
+	    		
+	    		function complete(id, o, args) {
+	    	        var id = id; // Transaction ID.
+	    	        var data = o.responseText; // Response data.
+	    	        var args = args[1]; // 'ipsum'.
+	    	        data = JSON.parse(data);
+	    	        data = data.map(function(item){
+	    	        	if(Lang.isString(item)){
+	    	        		item = {
+	    	        			name : item
+	    	        		}
+	    	        	}
+	    	        	return item;	    	        	
+	    	        });
+	    	        this.set("data", data);
+	    	    };
+	    		
+	    	    Y.on('io:complete', complete, Y, ['lorem', 'ipsum']);
+	    		var request = Y.io(uri);
+	    	}
+	    },
+	
+	    destructor : function() {},
+	
+	    renderUI : function() {},
+	
+	    bindUI : function() {
+	    	this.get(INPUT_NODE).on
+	    },
+	
+	    syncUI : function() {}
     });
     
     Y.AtWho = AtWho;
