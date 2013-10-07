ed8095b9382d1fa3a966d5beebc7c09752f52980
wjpgy@126.com <Administrator@WINDOWS-OF61AAN.mshome.net>
2013-10-08 00:57:07 +0800

    e

--- src/main/java/com/dm/system/SettingBean.java
+++ src/main/java/com/dm/system/SettingBean.java
@@ -5,18 +5,34 @@ import javax.inject.Inject;
 
 import com.dm.entity.GeneralProperty;
 import com.dm.service.SettingService;
+import javax.annotation.Resource;
+import javax.transaction.UserTransaction;
 
 @Model
 public class SettingBean {
+    
+        @Resource
+        UserTransaction utx;
 
 	@Inject
 	private SettingService settingService;
 	
-	public void saveGeneralProp(){
+	public void saveGeneralProp() throws Exception{
+            try{
+                utx.begin();
 		GeneralProperty entity = new GeneralProperty();
 		entity.setName("url");
 		entity.setValue("123");
 		settingService.create(entity);
+                utx.commit();
+            } catch (Exception e){
+                try {
+                    utx.rollback();
+                } catch (Exception re) {
+                   re.printStackTrace();
+                }
+                e.printStackTrace();                
+            }
 	}
 	
 }
