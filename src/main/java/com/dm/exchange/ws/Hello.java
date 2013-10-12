b3aaf96e13d3b716c0d8ec9ee9aa89f286459712
guoyong peng <wjpgy@126.com>
2013-10-12 16:45:57 +0800

    e

---
+++ src/main/java/com/dm/exchange/ws/Hello.java
@@ -0,0 +1,19 @@
+package com.dm.exchange.ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class Hello {
+	
+	private String message = new String("Hello, ");
+
+    public  Hello() {
+    }
+
+    @WebMethod
+    public String sayHello(String name) {
+        return message + name + ".";
+    }
+
+}
