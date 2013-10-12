8fceb6c5f43238d4a0dab18bd645179833f147cb
guoyong peng <wjpgy@126.com>
2013-10-12 10:45:19 +0800

    e

---
+++ src/main/java/com/dm/exchange/rest/ApplicationConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dm.exchange.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author cmp
+ */
+@ApplicationPath("/webapi")
+public class ApplicationConfig extends Application{
+    
+}
