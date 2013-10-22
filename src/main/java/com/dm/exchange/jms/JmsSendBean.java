58e327ba4a5c30b6c48f86e8592f5f245c09fa08
guoyong peng <wjpgy@126.com>
2013-10-22 16:43:36 +0800

    e

---
+++ src/main/java/com/dm/exchange/jms/JmsSendBean.java
@@ -0,0 +1,22 @@
+package com.dm.exchange.jms;
+
+import javax.annotation.Resource;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
+
+@Model
+public class JmsSendBean {
+	
+	@Inject
+	JMSContext context; 
+	
+	@Resource(mappedName = "java:global/jms/pointsQueue")
+	Queue pointsQueue;
+
+	public void sendMessage(){
+		context.createProducer().send(pointsQueue, "hello jms");
+	}
+	
+}
---
+++ src/main/java/com/dm/exchange/jms/SimpleMessageBean.java
@@ -0,0 +1,22 @@
+package com.dm.exchange.jms;
+
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+@MessageDriven(mappedName = "java:global/jms/pointsQueue")
+public class SimpleMessageBean implements MessageListener{
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			String msg = message.getBody(String.class);
+			System.out.println(msg);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}	
+	
+}
