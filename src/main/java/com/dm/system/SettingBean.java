4c0c389cbc9c47de9ba4a840585724ba57c57df7
guoyong peng <wjpgy@126.com>
2013-11-05 14:55:42 +0800

    e

--- src/main/java/com/dm/system/SettingBean.java
+++ src/main/java/com/dm/system/SettingBean.java
@@ -5,14 +5,18 @@ import javax.inject.Inject;
 
 import com.dm.entity.Setting;
 import com.dm.service.SettingService;
+import java.util.logging.Logger;
 
 @Model
 public class SettingBean {
+    
+        private static final Logger logger = Logger.getLogger(SettingBean.class.getName());
 
 	@Inject
 	private SettingService settingService;
 	
 	public void saveGeneralProp() throws Exception{
+            logger.info("test message");
             Setting entity = new Setting();
             entity.setName("url");
             entity.setValue("123");
