4edd83f837e0a0cc25620f2109500fed202cc5e6
guoyong peng <wjpgy@126.com>
2013-11-12 21:57:08 +0800

    e

--- src/main/webapp/resources/js/location.js
+++ src/main/webapp/resources/js/location.js
@@ -1,7 +1,74 @@
-YUI({filter : "raw"}).use("jsonp",function(Y){
+/*YUI().use("jsonp",function(Y){
 	var url="http://api.dangqian.com/apidiqu2/api.asp?format=json&id=000000000000&callback='{callback}'";
 	function handleJSONP(response){
 		console.log(response);
 	}
 	Y.jsonp(url,handleJSONP);	
+});*/
+YUI({
+	insertBefore: 'my_styles'
+}).use("event-base", "yui2-treeview", function(Y){	
+	
+	var YAHOO = Y.YUI2;
+	
+	var tree; //will hold our TreeView instance
+	
+	function treeInit() {
+		
+		YAHOO.log("Example's treeInit function firing.", "info", "example");
+		
+		//Hand off ot a method that randomly generates tree nodes:
+		buildRandomTextNodeTree();
+		
+		//handler for expanding all nodes
+		YAHOO.util.Event.on("expand", "click", function(e) {
+			YAHOO.log("Expanding all TreeView  nodes.", "info", "example");
+			tree.expandAll();
+			YAHOO.util.Event.preventDefault(e);
+		});
+		
+		//handler for collapsing all nodes
+		YAHOO.util.Event.on("collapse", "click", function(e) {
+			YAHOO.log("Collapsing all TreeView  nodes.", "info", "example");
+			tree.collapseAll();
+			YAHOO.util.Event.preventDefault(e);
+		});
+	}
+	
+	//This method will build a TreeView instance and populate it with
+	//between 3 and 7 top-level nodes
+	function buildRandomTextNodeTree() {
+	
+		//instantiate the tree:
+		tree = new YAHOO.widget.TreeView("treeDiv1");
+		
+		//create top-level nodes
+		for (var i = 0; i < Math.floor((Math.random()*4) + 3); i++) {
+			var tmpNode = new YAHOO.widget.TextNode("label-" + i, tree.getRoot(), false);
+			
+			//we'll delegate to another function to build child nodes:
+			buildRandomTextBranch(tmpNode);
+		}
+		
+		//once it's all built out, we need to render
+		//our TreeView instance:
+		tree.draw();
+	}
+
+	//This function adds a random number <4 of child nodes to a given
+	//node, stopping at a specific node depth:
+	function buildRandomTextBranch(node) {
+		if (node.depth < 6) {
+			YAHOO.log("buildRandomTextBranch: " + node.index);
+			for ( var i = 0; i < Math.floor(Math.random() * 4) ; i++ ) {
+				var tmpNode = new YAHOO.widget.TextNode(node.label + "-" + i, node, false);
+				buildRandomTextBranch(tmpNode);
+			}
+		}
+	}
+	
+    Y.on('domready', function () {
+    	treeInit();
+    });
+
 });
