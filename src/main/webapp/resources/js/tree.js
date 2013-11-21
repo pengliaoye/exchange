67d71b569547be08c530e9fdc2b2a4b228483a1d
wjpgy@126.com <Administrator@WINDOWS-OF61AAN.mshome.net>
2013-11-21 21:02:01 +0800

    e

--- src/main/webapp/resources/js/tree.js
+++ src/main/webapp/resources/js/tree.js
@@ -1,10 +1,3 @@
-/*YUI().use("jsonp",function(Y){
-	var url="http://api.dangqian.com/apidiqu2/api.asp?format=json&id=000000000000&callback='{callback}'";
-	function handleJSONP(response){
-		console.log(response);
-	}
-	Y.jsonp(url,handleJSONP);	
-});*/
 YUI({
 	insertBefore: 'my_styles'
 }).use("event-base", "yui2-treeview", function(Y){	
