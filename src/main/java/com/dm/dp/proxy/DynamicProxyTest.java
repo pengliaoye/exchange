dbfc79b69ccf598fd12ba41f8ff3467c274ff0a5
guoyong peng <wjpgy@126.com>
2014-02-17 10:56:06 +0800

    e

--- src/main/java/com/dm/dp/proxy/DynamicProxyTest.java
+++ src/main/java/com/dm/dp/proxy/DynamicProxyTest.java
@@ -1,10 +1,8 @@
 package com.dm.dp.proxy;
 
-import org.junit.Test;
-
 public class DynamicProxyTest {
-	@Test
-	public void testDynamicProxy() {
+	
+	public static void main(String args) {
 		Foo foo = (Foo) DynamicProxy.newInstance(new FooImpl());
 		System.out.println(foo.bar(null));
 	}
