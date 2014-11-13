5864586ce3cb4902ea1e8b841e531175b46109df
guoyong peng <guoyong.peng@digicompass.com>
2014-11-13 15:34:19 +0800

    e

--- src/main/java/com/dm/demo/ExampleClient.java
+++ src/main/java/com/dm/demo/ExampleClient.java
@@ -1,54 +1,52 @@
 package com.dm.demo;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.recipes.leader.LeaderSelector;
-import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+public class ExampleClient extends LeaderSelectorListenerAdapter implements Closeable {
 
-public class ExampleClient extends LeaderSelectorListenerAdapter implements
-		Closeable {
+  private final String name;
+  private final LeaderSelector leaderSelector;
+  private final AtomicInteger leaderCount = new AtomicInteger();
 
-	private final String name;
-	private final LeaderSelector leaderSelector;
-	private final AtomicInteger leaderCount = new AtomicInteger();
+  public ExampleClient(CuratorFramework client, String path, String name) {
+    this.name = name;
 
-	public ExampleClient(CuratorFramework client, String path, String name) {
-		this.name = name;
+    leaderSelector = new LeaderSelector(client, path, this);
 
-		leaderSelector = new LeaderSelector(client, path, this);
+    leaderSelector.autoRequeue();
+  }
 
-		leaderSelector.autoRequeue();
-	}
+  public void start() throws IOException {
+    leaderSelector.start();
+  }
 
-	public void start() throws IOException {
-		leaderSelector.start();
-	}
+  @Override
+  public void takeLeadership(CuratorFramework client) throws Exception {
+    final int waitSeconds = (int) (5 * Math.random()) + 1;
 
-	@Override
-	public void takeLeadership(CuratorFramework client) throws Exception {
-		final int waitSeconds = (int) (5 * Math.random()) + 1;
+    System.out.println(name + " is now the leader. Waiting " + waitSeconds + " seconds...");
+    System.out.println(name + " has been leader " + leaderCount.getAndIncrement()
+        + " time(s) before.");
+    try {
+      Thread.sleep(TimeUnit.SECONDS.toMillis(waitSeconds));
+    } catch (InterruptedException e) {
+      System.err.println(name + " was interrupted.");
+      Thread.currentThread().interrupt();
+    } finally {
+      System.out.println(name + " relinquishing leadership.\n");
+    }
+  }
 
-		System.out.println(name + " is now the leader. Waiting " + waitSeconds
-				+ " seconds...");
-		System.out.println(name + " has been leader "
-				+ leaderCount.getAndIncrement() + " time(s) before.");
-		try {
-			Thread.sleep(TimeUnit.SECONDS.toMillis(waitSeconds));
-		} catch (InterruptedException e) {
-			System.err.println(name + " was interrupted.");
-			Thread.currentThread().interrupt();
-		} finally {
-			System.out.println(name + " relinquishing leadership.\n");
-		}
-	}
-
-	@Override
-	public void close() throws IOException {
-		leaderSelector.close();
-	}
+  @Override
+  public void close() throws IOException {
+    leaderSelector.close();
+  }
 
 }
