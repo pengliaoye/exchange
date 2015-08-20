130281767b49a6e5380def096e509940402e2591
guoyong peng <guoyong.peng@digicompass.com>
2015-08-20 16:40:00 +0800

    e

--- src/main/java/com/dm/entity/Sport.java
+++ src/main/java/com/dm/entity/Sport.java
@@ -17,4 +17,20 @@ public class Sport {
 	@Size(min = 1, max = 200)
 	private String name;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
