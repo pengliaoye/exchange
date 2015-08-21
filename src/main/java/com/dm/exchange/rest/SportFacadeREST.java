e19a50b4d8a37b20cc598f6fc88f7a840dbd9cc2
guoyong peng <guoyong.peng@digicompass.com>
2015-08-21 10:06:53 +0800

    e

--- src/main/java/com/dm/exchange/rest/SportFacadeREST.java
+++ src/main/java/com/dm/exchange/rest/SportFacadeREST.java
@@ -6,8 +6,13 @@ import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.dm.entity.Sport;
@@ -17,11 +22,39 @@ import com.dm.entity.Sport;
 @Path("sports")
 public class SportFacadeREST extends AbstractFacade<Sport> {
 
-	@PersistenceContext(unitName="default")
-	protected EntityManager em;
+	@PersistenceContext
+	protected EntityManager entityManager;
 
 	public SportFacadeREST() {
 		super(Sport.class);
+	}
+
+	@POST
+	@Override
+	@Consumes({ "application/xml", "application/json" })
+	public void create(Sport sport) {
+		super.create(sport);
+	}
+
+	@PUT
+	@Override
+	@Consumes({ "application/xml", "application/json" })
+	@Path("{id}")
+	public void edit(Sport sport) {
+		super.edit(sport);
+	}
+
+	@DELETE
+	@Path("{id}")
+	public void remove(@PathParam("id") Integer id) {
+		super.remove(super.find(id));
+	}
+
+	@GET
+	@Path("{id}")
+	@Produces({ "application/xml", "application/json" })
+	public Sport find(@PathParam("id") Integer id) {
+		return super.find(id);
 	}
 
 	@GET
@@ -31,9 +64,23 @@ public class SportFacadeREST extends AbstractFacade<Sport> {
 		return super.getAll();
 	}
 
+	@GET
+	@Path("{from}/{to}")
+	@Produces({ "application/xml", "application/json" })
+	public List<Sport> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+		return super.findRange(new int[] { from, to });
+	}
+
+	@GET
+	@Path("count")
+	@Produces("text/plain")
+	public String getCount() {
+		return String.valueOf(super.count());
+	}
+
 	@Override
 	protected EntityManager getEntityManager() {
-		return em;
+		return entityManager;
 	}
 
 }
